cb1cee09153ee28409b2405134f4b0e2c36c4657
amanc1361 <rahymrm@gmail.com>
2025-07-31 16:54:47 +0330

    change next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,33 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//    reactStrictMode: true, 
 
-
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'nerrkhin.com', 
-//         port: '', 
-//         pathname: '/images/**', 
-//       },
-     
-//     ],
-//   },
-// };
-
-// module.exports = nextConfig;
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
+  output: 'standalone',
   async rewrites() {
     if (process.env.NODE_ENV === 'development') {
       return [
         {
             source: '/api/go/:path*',
-          destination: 'http://localhost:8084/v1/:path*',
+            destination: 'http://localhost:8084/v1/:path*',
         },
       ];
     }
