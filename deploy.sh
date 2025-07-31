faece5e7c36c8fe7801507cad3fd25ddd7f639e8
amanc1361 <rahymrm@gmail.com>
2025-07-31 18:46:55 +0330

    change change

--- deploy.sh
+++ deploy.sh
@@ -5,23 +5,25 @@ set -e
 DEPLOY_TAG=$1
 echo "🔖 Deploying version: $DEPLOY_TAG"
 
-# مسیر فایل‌ها را مشخص کن
+# مشخص‌کردن نام فایل‌ها
 BACKEND_IMAGE="backend-${DEPLOY_TAG}.tar"
 FRONTEND_IMAGE="frontend-${DEPLOY_TAG}.tar"
 BACKEND_ENV="backend.env"
 FRONTEND_ENV="frontend.env"
 
+# رفتن به دایرکتوری پکیج
 cd ~/nerkhin/deploy_package
 
 echo "📦 Loading Docker images..."
-docker load -i "$BACKEND_IMAGE"
-docker load -i "$FRONTEND_IMAGE"
+docker load -i $BACKEND_IMAGE
+docker load -i $FRONTEND_IMAGE
 
-# کپی فایل‌های env به ریشه پروژه
-cp "$BACKEND_ENV" ../.env.backend
-cp "$FRONTEND_ENV" ../.env.frontend
+# کپی envها برای استفاده توسط docker-compose
+cp $BACKEND_ENV ../.env.backend
+cp $FRONTEND_ENV ../.env.frontend
 
+# رفتن به ریشه پروژه
 cd ..
 
 echo "🚀 Running Docker Compose..."
-DEPLOY_TAG="$DEPLOY_TAG" docker compose -f deploy_package/docker-compose.template.yml up -d --remove-orphans --build
+DEPLOY_TAG=$DEPLOY_TAG docker compose -f deploy_package/docker-compose.template.yml up -d --remove-orphans
