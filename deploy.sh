86f3a17e0693ce300ac7d777eb7ca5c1093d5074
amanc1361 <rahymrm@gmail.com>
2025-07-31 18:22:02 +0330

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,25 @@
 #!/bin/bash
-
 set -e
 
 SHA_TAG=$1
 echo "🔖 Deploying version: $SHA_TAG"
 
-# مسیر فایل‌ها را مشخص کن
+cd ~/nerkhin/deploy_package
+
+# تعیین نام فایل‌ها
 BACKEND_IMAGE="backend-${SHA_TAG}.tar"
 FRONTEND_IMAGE="frontend-${SHA_TAG}.tar"
 BACKEND_ENV="backend.env"
 FRONTEND_ENV="frontend.env"
 
-# آماده‌سازی دایرکتوری اجرا
-cd ~/nerkhin/deploy_package
-
 echo "📦 Loading Docker images..."
 docker load -i $BACKEND_IMAGE
 docker load -i $FRONTEND_IMAGE
 
-# کپی فایل‌های env به یک مسیر بالاتر برای docker-compose
+echo "📝 Copy env files..."
 cp $BACKEND_ENV ../.env.backend
 cp $FRONTEND_ENV ../.env.frontend
 
-# رفتن به پوشه اصلی پروژه
-cd ..
-
-# 🔧 تعریف متغیر محیطی برای docker-compose
-export SHA_TAG=$SHA_TAG
-
-# اجرای compose
 echo "🚀 Running Docker Compose..."
-docker compose -f deploy_package/docker-compose.template.yml up -d --remove-orphans --build
+cd ..
+SHA_TAG=$SHA_TAG docker compose -f deploy_package/docker-compose.template.yml up -d --remove-orphans --build
