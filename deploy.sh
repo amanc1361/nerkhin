9fe00d1735d1c9a4e3f15d2b678bda550f6d9aeb
amanc1361 <rahymrm@gmail.com>
2025-08-01 23:17:15 +0330

    change deploy

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,15 @@
-#!/bin/bash
-
 set -e
-
 DEPLOY_TAG=$1
 echo "🔖 Deploying version: $DEPLOY_TAG"
-
 BACKEND_IMAGE="backend-${DEPLOY_TAG}.tar"
 FRONTEND_IMAGE="frontend-${DEPLOY_TAG}.tar"
 BACKEND_ENV="backend.env"
 FRONTEND_ENV="frontend.env"
-
-# رفتن به پوشه‌ای که فایل‌ها در آن قرار دارند
 cd "$(dirname "$0")"
-
 echo "📦 Loading Docker images..."
 docker load -i "$BACKEND_IMAGE"
 docker load -i "$FRONTEND_IMAGE"
-
-# کپی فایل‌های env در همین مسیر (برای استفاده در compose)
 cp "$BACKEND_ENV" .env
 cp "$FRONTEND_ENV" .env.frontend
-
 echo "🚀 Running Docker Compose..."
 DEPLOY_TAG=$DEPLOY_TAG docker compose -f docker-compose.template.yml up -d --remove-orphans --build
