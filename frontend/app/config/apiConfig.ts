eb1b16ed5cc0d39cf73ea4463d3cfde7fcc7f4f3
amanc1361 <rahymrm@gmail.com>
2025-08-06 09:01:54 +0330

    change env for auth

--- frontend/app/config/apiConfig.ts
+++ frontend/app/config/apiConfig.ts
@@ -4,7 +4,9 @@ import { absApiUrl } from "@/app/utils/absurl";
 //export const API_BASE_URL = "/api";
 
 /** آدرس کامل برای SSR/Node؛ در absApiUrl استفاده می‌شود */
-export const API_BASE_URL = "https://nerkhin.com/api"; 
+export const API_BASE_URL = "https://nerkhin.com/api/go";
+
+
 export const INTERNAL_GO_API_URL =
   process.env.INTERNAL_GO_API_URL || "https://nerkhin.com/api";
 
