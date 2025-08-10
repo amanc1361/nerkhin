a739d348651a46ca5c9ed1c3d162b0f525c6d452
amanc1361 <rahymrm@gmail.com>
2025-08-10 14:53:44 +0330

    change nginx for show images

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -11,9 +11,9 @@ const nextConfig = {
   images: {
     remotePatterns: [
       // پروDUCTION
-      { protocol: 'https', hostname: 'nerkhin.com', pathname: '/images/**' },
+      { protocol: 'https', hostname: 'nerkhin.com', pathname: '/uploads/**' },
       // DEV (اگر تصاویر را از بک‌اند لوکال می‌گیرید)
-      { protocol: 'http', hostname: 'localhost', port: '8080', pathname: '/images/**' },
+      { protocol: 'http', hostname: 'localhost', port: '8080', pathname: '/uploads/**' },
     ],
   },
 };
