ad34528cde4ec3f367eb165f57e9a4c2e1f13fca
amanc1361 <rahymrm@gmail.com>
2025-08-19 19:45:59 +0330

    change product struct

--- backend/scripts/db/migrations/00000001_create_product_table.up.sql
+++ backend/scripts/db/migrations/00000001_create_product_table.up.sql
@@ -13,20 +13,20 @@ CREATE TABLE IF NOT EXISTS product_brand (
 
 
 CREATE TABLE IF NOT EXISTS product (
-	id 					BIGSERIAL 		NOT NULL PRIMARY KEY, 
-	model_name          VARCHAR(200) 	NOT NULL UNIQUE, 
-	brand_id 			BIGINT 			NOT NULL REFERENCES product_brand (id), 
-	default_image_url   VARCHAR(200)  	NULL,
-	description 		VARCHAR(500) 	NOT NULL,
-	images_count        INT             DEFAULT 0,
-	state_c 			SMALLINT 		NOT NULL,
-	likes_count   		INT        		NOT NULL,
-	shops_count   		INT        		NOT NULL,
-	created_at 			TIMESTAMP 		NOT NULL,
-	updated_at 			TIMESTAMP 		NOT NULL
-	
+  id                BIGSERIAL      PRIMARY KEY,
+  model_name        VARCHAR(200)   NOT NULL,
+  brand_id          BIGINT         NOT NULL REFERENCES product_brand (id),
+  default_image_url VARCHAR(200),
+  description       VARCHAR(500)   NOT NULL,
+  images_count      INT            DEFAULT 0,
+  state_c           SMALLINT       NOT NULL,
+  likes_count       INT            NOT NULL,
+  shops_count       INT            NOT NULL,
+  created_at        TIMESTAMP      NOT NULL,
+  updated_at        TIMESTAMP      NOT NULL,
+  CONSTRAINT uq_product_model_brand UNIQUE (model_name, brand_id)
 );
-
+CREATE INDEX IF NOT EXISTS idx_product_brand_id ON product(brand_id);
 CREATE TABLE IF NOT EXISTS product_image (
 	id 				BIGSERIAL 		NOT NULL PRIMARY KEY, 
 	product_id 		BIGINT 			NOT NULL REFERENCES product (id) ON DELETE CASCADE,
