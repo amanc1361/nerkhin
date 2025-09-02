fa14d3545888662c905bf3ef99cb038e63357ce0
amanc1361 <rahymrm@gmail.com>
2025-09-02 21:04:15 +0330

    change filter table

--- backend/scripts/db/migrations/00000009_create_product_filter_table.up.sql
+++ backend/scripts/db/migrations/00000009_create_product_filter_table.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE IF NOT EXISTS product_filter (
   id                BIGSERIAL     NOT NULL PRIMARY KEY,
   category_id       BIGINT        NOT NULL REFERENCES product_category (id),
-  name              VARCHAR(50)   NOT NULL,
-  display_name      VARCHAR(50)   NOT NULL
+  name              VARCHAR(150)   NOT NULL,
+  display_name      VARCHAR(150)   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS product_filter_option (
   id                BIGSERIAL     NOT NULL PRIMARY KEY,
   filter_id         BIGINT        NOT NULL REFERENCES product_filter (id) ON DELETE CASCADE,
-  name              VARCHAR(50)   NOT NULL
+  name              VARCHAR(150)   NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS product_filter_relation (
